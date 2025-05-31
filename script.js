e5f9fea5a11448f6c7a8cc825dcd480e5f88d60d
BirdiesYT2 <cmbird12@gmail.com>
2025-05-31 13:28:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function search() {
     }
   }
 }
+
+var links = new Array();
+links[0] = "level/1";
+links[1] = "level/2";
+
+function openLink() {
+  // Chooses a random link:
+  var i = Math.floor(Math.random() * links.length);
+  // Directs the browser to the chosen target:
+  parent.location = links[i];
+  return false;
+}
